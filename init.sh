76c30cfef98936ef8694594fb0fbffb337170f89
Rex Truong <58905307+aratare-jp@users.noreply.github.com>
2021-01-27 21:42:50 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-echo "Installing tools with this script requires sudo. Would you like to continue? [Y/n]"
-echo "Installing tools..."
-sudo su
+read -p "Installing tools with this script may require sudo. Would you like to continue? [Y/n]" continue
+if [ $continue = 'Y' ] || [ $continue = '' ]
+  sudo su
+then
+elif [ $continue = 'n' ]
+  echo "Exiting"
+  exit 0
+else
+  echo "Unknown answer"
+  exit 1
+fi
+
+echo "Updating"
 apt-get update
+apt-get upgrade
+
+echo "Installing ZSH"
+apt-get install zsh
+
+echo "Installing OhMyZSH"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing homebrew"
