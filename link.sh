2bedb52fe80f4b09ae750111ff625ecff91c9980
Rex Truong <58905307+aratare-jp@users.noreply.github.com>
2021-12-14 16:26:46 +0700

    Added link.sh

---
+++ link.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf ~/.config/i3
+ln -s $(pwd)/.config/i3 ~/.config/i3
+
+rm -rf ~/.config/polybar
+ln -s $(pwd)/.config/polybar ~/.config/polybar
+
+rm -rf ~/.config/dunst
+ln -s $(pwd)/.config/dunst ~/.config/dunst
+
