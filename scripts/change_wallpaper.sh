a3157f7df9d9e73c4fa29892032d43f7f051147b
Kazuhiko Rei <58905307+aratare-jp@users.noreply.github.com>
2023-01-31 00:44:20 +0900

    Added wallpaper changer

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 {
 while true; do
-    DISPLAY=:0 feh --randomize --bg-fill --no-fehbg ~/Pictures/4k
+    DISPLAY=:0 feh --randomize --bg-fill --no-fehbg ~/Pictures/3440x1400
     sleep 5m
 done
 } &
