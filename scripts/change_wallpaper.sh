3e8e32ecc09305a148bcafe99db10ca0062e4e54
Kazuhiko Rei <58905307+aratare-jp@users.noreply.github.com>
2023-02-01 22:49:28 +0900

    Fixed wrong directory for wallpaper

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 {
 while true; do
-    feh --randomize --bg-fill --no-fehbg $HOME/Pictures/3440x1400
+    feh --randomize --bg-fill --no-fehbg $HOME/Pictures/3440x1440
     sleep 5m
 done
 } &
