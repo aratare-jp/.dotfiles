bd5df1c3e5cc0ecd9b0c87ed85e9a7fb6c356ce1
Kazuhiko Rei <58905307+aratare-jp@users.noreply.github.com>
2023-02-05 01:55:32 +0900

    Fixed calendar

--- .config/polybar/popup-calendar.sh
+++ .config/polybar/popup-calendar.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-BAR_HEIGHT=22  # polybar height
-BORDER_SIZE=1  # border size from your wm settings
-YAD_WIDTH=222  # 222 is minimum possible value
-YAD_HEIGHT=193 # 193 is minimum possible value
+BAR_HEIGHT=45  # polybar height
+BORDER_SIZE=0  # border size from your wm settings
+YAD_WIDTH=300  # 222 is minimum possible value
+YAD_HEIGHT=200 # 193 is minimum possible value
 DATE="$(date +"%a %d %H:%M")"
 
 case "$1" in
@@ -26,7 +26,7 @@ case "$1" in
 
     # Y
     if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
-        : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
+        : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE - 60))
     else #Top
         : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
     fi
