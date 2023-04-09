aabfc95d418177bdad7e37f263c10ff9d288330e
Rei <t-khoi.truong@paypay-corp.co.jp>
2023-04-10 08:11:12 +0900

    Update

---
+++ .zsh.d/common/01_omz.zsh
@@ -0,0 +1,14 @@
+export ZSH="$HOME/.oh-my-zsh"
+
+plugins=(
+    git
+    fast-syntax-highlighting
+    zsh-autosuggestions
+    fzf-tab
+    docker
+    docker-compose
+    gradle
+)
+
+source $ZSH/oh-my-zsh.sh
+
